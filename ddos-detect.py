1f63616539b6a4eec2bfbeacc8a6e45e3019425d
Jacobvs <jacobvs2@gmail.com>
2017-12-12 22:42:17 -0800

    Fixing labels and shape of model

--- ddos-detect.py
+++ ddos-detect.py
@@ -2,7 +2,8 @@ import os
 
 import arff
 import numpy as np
-from keras.layers import Dense, Dropout
+from numpy import argmax
+from keras.layers import Dense, Dropout, Flatten
 from keras.models import Sequential
 from keras.utils import to_categorical
 
@@ -22,17 +23,19 @@ file = open("data/final-dataset-short.arff", 'r')
 
 
 def generate_model(shape):
+
     model = Sequential()
 
-    model.add(Dense(64, input_dim=shape, activation='relu'))
+    model.add(Dense(26, input_shape=(shape, 27), activation='relu'))
     model.add(Dropout(0.15))
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.25))
-    model.add(Dense(128, activation='relu'))
+    model.add(Dense(45, activation='relu'))
     model.add(Dropout(0.2))
     model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(45, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(26, activation='relu'))
+    print(model.summary())
     model.add(Dropout(0.15))
     model.add(Dense(1, activation='sigmoid'))
 
@@ -46,11 +49,17 @@ def scrape_data():
 
     vals = [val[0: -1] for val in data['data']]
     labels = [label[-1] for label in data['data']]
-    labels = to_categorical(labels, 5)
+
+    #flatten labels to integer outputs
+    #labels = to_categorical(labels, 5)
+    #abels = argmax(labels[0])
+
     training_data = vals[0: int(.9 * len(vals))]
     training_labels = labels[0: int(.9 * len(vals))]
     validation_data = vals[int(.9 * len(vals)):]
     validation_labels = vals[int(.9 * len(vals)):]
+
+    print(training_labels)
 
     np.save('saved-data/vals', np.asarray(vals))
     np.save('saved-data/labels', np.asarray(labels))
@@ -67,10 +76,10 @@ data_eval = np.load('saved-data/validation_data.npy')
 label_eval = np.load('saved-data/validation_labels.npy')
 
 
-model = generate_model(data_train.shape[1])
-model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
+model = generate_model(data_train.shape[0])
+model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-model.fit(data_train, label_train, epochs=20)
+model.fit(data_train, label_train, epochs=5)
 
 print(model.evaluate(data_eval, label_eval))
 
