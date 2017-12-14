c569e491855ffc879a5e78ad63699dbafe34c334
Jacobvs <jacobvs2@gmail.com>
2017-12-14 02:49:09 -0800

    cleaned up code added comments added visualization added a save of the model

--- ddos-detect.py
+++ ddos-detect.py
@@ -2,105 +2,114 @@ import os
 
 import arff
 import numpy as np
-from numpy import argmax
-from keras.layers import Dense, Dropout, Flatten
+from matplotlib import pyplot as plt
+from keras.layers import Dense, Dropout
 from keras.models import Sequential
 from keras.utils import to_categorical
+from keras.utils import plot_model
 
+# set the directory of the dataset
 file = open("data/final-dataset.arff", 'r')
 
 
-# print(type(d))
-
-# file = open('data/final dataset.arff', 'r')
-
-# d = decoder.decode(file.read(), encode_nominal=True)
-# pprint.pprint(d)
-#
-# dataset = arff.load(open('data/final dataset.arff', 'rb'))
-# #data = dataset['data']
-# pprint.pprint(dataset)
-
-
 def generate_model(shape):
-    print(shape)
 
+    # define the model
     model = Sequential()
 
-    model.add(Dense(22, input_dim=shape, activation='relu'))
-    model.add(Dropout(0.15))
-    model.add(Dense(45, activation='relu'))
-    model.add(Dropout(0.2))
+    model.add(Dense(22, input_dim=shape, kernel_initializer='uniform', activation='relu'))
+    model.add(Dropout(0.4))
     model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.25))
-    model.add(Dense(45, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(26, activation='relu'))
-    print(model.summary())
-    model.add(Dropout(0.15))
-    model.add(Dense(5, activation='sigmoid'))
+    model.add(Dropout(0.4))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(5, activation='softmax'))
     print(model.summary())
 
     return model
 
 
 def scrape_data():
-    # outputs d as a dictionary
+    # decode the .arff data and change text labels into numerical
     decoder = arff.ArffDecoder()
     data = decoder.decode(file, encode_nominal=True)
 
+    # split the raw data into data and labels
     vals = [val[0: -1] for val in data['data']]
     labels = [label[-1] for label in data['data']]
 
-    #flatten labels to integer outputs
-    #labels = to_categorical(labels, 5)
-    #abels = argmax(labels[0])
-
+    # split the labels and data into traning and validation sets
     training_data = vals[0: int(.9 * len(vals))]
     training_labels = labels[0: int(.9 * len(vals))]
     validation_data = vals[int(.9 * len(vals)):]
     validation_labels = labels[int(.9 * len(vals)):]
 
-    print(training_labels)
+    # flatten labels with one hot encoding
     training_labels = to_categorical(training_labels, 5)
     validation_labels = to_categorical(validation_labels, 5)
-    print(training_labels.shape)
 
-    np.save('saved-data/vals', np.asarray(vals))
-    np.save('saved-data/labels', np.asarray(labels))
-    np.save('saved-data/training_data', np.asarray(training_data))
-    np.save('saved-data/validation_data', np.asarray(validation_data))
-    np.save('saved-data/training_labels', np.asarray(training_labels))
-    np.save('saved-data/validation_labels', np.asarray(validation_labels))
+    # save all arrays with numpy
+    np.save('saved-files/vals', np.asarray(vals))
+    np.save('saved-files/labels', np.asarray(labels))
+    np.save('saved-files/training_data', np.asarray(training_data))
+    np.save('saved-files/validation_data', np.asarray(validation_data))
+    np.save('saved-files/training_labels', np.asarray(training_labels))
+    np.save('saved-files/validation_labels', np.asarray(validation_labels))
 
-#scrape_data()
-if not os.path.exists('saved-data/labels'):
+
+# check to see if saved data exists, if not then create the data
+if not os.path.exists('saved-files/training_data.npy') or not os.path.exists(
+        'saved-files/training_labels.npy') or not os.path.exists(
+        'saved-files/validation_data.npy') or not os.path.exists('saved-files/validation_labels.npy'):
     print('creating')
-    if not os.path.exists('saved-data'):
-        os.mkdir('saved-data')
+    if not os.path.exists('saved-files'):
+        os.mkdir('saved-files')
     scrape_data()
 
-data_train = np.load('saved-data/training_data.npy')
-label_train = np.load('saved-data/training_labels.npy')
+# load the saved data
+data_train = np.load('saved-files/training_data.npy')
+label_train = np.load('saved-files/training_labels.npy')
+data_eval = np.load('saved-files/validation_data.npy')
+label_eval = np.load('saved-files/validation_labels.npy')
 
-data_eval = np.load('saved-data/validation_data.npy')
-label_eval = np.load('saved-data/validation_labels.npy')
-
-print(data_train[0])
-
+# generate and compile the model
 model = generate_model(len(data_train[0]))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(data_train, label_train, epochs=5)
+# only using 3 epochs otherwise the model would overfit to the data
+history = model.fit(data_train, label_train, validation_data=(data_eval, label_eval), epochs=3)
 
+# evaluating the model's performace
 print(model.evaluate(data_eval, label_eval))
+
+plot_model(model, to_file='model.png', show_shapes=True)
+
+plt.figure(1)
+
+# summarize history for accuracy
+plt.subplot(211)
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+
+# summarize history for loss
+plt.subplot(212)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# save the model for later so no retraining is needed
+model.save('saved-files/model.h5')
 
 # play sound when done with code to alert me
 os.system('afplay /System/Library/Sounds/Ping.aiff')
 os.system('afplay /System/Library/Sounds/Ping.aiff')
-
-# model.compile(optimizer='sgd')
-#
-# model.fit_generator(data, samples_per_epoch=50, nb_epoch=10, batch_size=10)
-#
-# model.predict()
