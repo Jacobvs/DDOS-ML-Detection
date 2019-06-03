40d55edc86eef081bf212931bc3865a642582cab
Jacobvs <jacobvs2@gmail.com>
2019-06-02 17:02:00 -0700

    Added model accuracy plot & updated data

--- ddos-detect.py
+++ ddos-detect.py
@@ -5,26 +5,34 @@ import numpy as np
 from matplotlib import pyplot as plt
 from keras.layers import Dense, Dropout
 from keras.models import Sequential
+from keras.models import load_model
+from keras.callbacks import TensorBoard
 from keras.utils import to_categorical
 from keras.utils import plot_model
 
 # set the directory of the dataset
 file = open("data/final-dataset.arff", 'r')
 
+# # Togglable Options
+# regenerate_model = False
+# regenerate_data = False
+# generate_graphs = True
+# save_model = True
+# create_model_image = False
+
 
 def generate_model(shape):
-
     # define the model
     model = Sequential()
 
-    model.add(Dense(22, input_dim=shape, kernel_initializer='uniform', activation='relu'))
+    model.add(Dense(30, input_dim=shape, kernel_initializer='uniform', activation='relu'))
     model.add(Dropout(0.4))
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(10, activation='relu'))
     model.add(Dropout(0.4))
-    model.add(Dense(128, activation='relu'))
+    model.add(Dense(10, activation='relu'))
     model.add(Dropout(0.4))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.4))
+    # model.add(Dense(64, activation='relu'))
+    # model.add(Dropout(0.4))
     model.add(Dense(5, activation='softmax'))
     print(model.summary())
 
@@ -40,11 +48,18 @@ def scrape_data():
     vals = [val[0: -1] for val in data['data']]
     labels = [label[-1] for label in data['data']]
 
+    for val in labels:
+        if labels[val] != 0:
+            labels[val] = 1
+
     # split the labels and data into traning and validation sets
     training_data = vals[0: int(.9 * len(vals))]
     training_labels = labels[0: int(.9 * len(vals))]
     validation_data = vals[int(.9 * len(vals)):]
     validation_labels = labels[int(.9 * len(vals)):]
+
+
+    print(training_labels)
 
     # flatten labels with one hot encoding
     training_labels = to_categorical(training_labels, 5)
@@ -60,13 +75,15 @@ def scrape_data():
 
 
 # check to see if saved data exists, if not then create the data
-if not os.path.exists('saved-files/training_data.npy') or not os.path.exists(
-        'saved-files/training_labels.npy') or not os.path.exists(
-        'saved-files/validation_data.npy') or not os.path.exists('saved-files/validation_labels.npy'):
-    print('creating')
-    if not os.path.exists('saved-files'):
-        os.mkdir('saved-files')
-    scrape_data()
+# if not os.path.exists('saved-files/training_data.npy') or not os.path.exists(
+#         'saved-files/training_labels.npy') or not os.path.exists(
+#     'saved-files/validation_data.npy') or not os.path.exists('saved-files/validation_labels.npy'):
+#     print('creating')
+#     if not os.path.exists('saved-files'):
+#         os.mkdir('saved-files')
+#     scrape_data()
+scrape_data()
+
 
 # load the saved data
 data_train = np.load('saved-files/training_data.npy')
@@ -78,12 +95,23 @@ label_eval = np.load('saved-files/validation_labels.npy')
 model = generate_model(len(data_train[0]))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# initialize tensorboard
+tensorboard = TensorBoard(log_dir='logs/', histogram_freq=0, write_graph=True, write_images=True)
+
 # only using 3 epochs otherwise the model would overfit to the data
-history = model.fit(data_train, label_train, validation_data=(data_eval, label_eval), epochs=3)
+history = model.fit(data_train, label_train, validation_data=(data_eval, label_eval), epochs=2, callbacks=[tensorboard])
+loss_history = history.history["loss"]
+
+numpy_loss_history = np.array(loss_history)
+np.savetxt("saved-files/loss_history.txt", numpy_loss_history, delimiter=",")
+
+model = load_model('saved-files/model.h5')
 
 # evaluating the model's performace
 print(model.evaluate(data_eval, label_eval))
+print(model.evaluate(data_train, label_train))
 
+#if create_model_image:
 plot_model(model, to_file='model.png', show_shapes=True)
 
 plt.figure(1)
@@ -108,7 +136,7 @@ plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 
 # save the model for later so no retraining is needed
-model.save('saved-files/model.h5')
+# model.save('saved-files/model.h5')
 
 # play sound when done with code to alert me
 os.system('afplay /System/Library/Sounds/Ping.aiff')
